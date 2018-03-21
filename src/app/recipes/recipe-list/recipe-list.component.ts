f1515d40eb90302f9be226325d68eed6903e0935
Marko Berger <marko.berger@gmail.com>
2018-03-21 12:15:23 +0100

    Add bootstrap to components , finished init component setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
-  selector: 'app-recipe-list',
+selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Cufte', 'Cufte italiano', 'http://wall.hr/cdn/uploads/850x/2012/09/aac8366c/becki-snicel.jpg'),
+    new Recipe('Cufte', 'Cufte italiano', 'http://wall.hr/cdn/uploads/850x/2012/09/aac8366c/becki-snicel.jpg')
+    
+  ];
+  
 
   constructor() { }
 
