552bda47367eceeb03f2bedbc75943eb91d32a5a
Marko Berger <marko.berger@gmail.com>
2018-03-21 23:24:50 +0100

    Add cross component communication , little unstable

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -10,14 +11,18 @@ selector: 'app-recipe-list',
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Cufte', 'Cufte italiano', 'http://wall.hr/cdn/uploads/850x/2012/09/aac8366c/becki-snicel.jpg'),
-    new Recipe('Cufte', 'Cufte italiano', 'http://wall.hr/cdn/uploads/850x/2012/09/aac8366c/becki-snicel.jpg')
+    new Recipe('Cufteeeee', 'Cufte italiano', 'http://wall.hr/cdn/uploads/850x/2012/09/aac8366c/becki-snicel.jpg')
     
   ];
   
+  @Output() recipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onEmitedRecipe(recipeEmited: Recipe){
+    this.recipe.emit(recipeEmited);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -20,4 +20,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient );
+  }
 }
