4864a94cd0176712dbab0cba30b79f475e9565ac
Marko Berger <marko.berger@gmail.com>
2018-03-21 21:32:03 +0100

    Add primitive navigatione

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -7,5 +7,9 @@ import {Component} from '@angular/core';
 })
 
 export class HeaderComponent {
+    @Output() component = new EventEmitter<string>(); 
 
+    onSelect(pageComponent: string){
+        this.component.emit(pageComponent);
+    }
 }
